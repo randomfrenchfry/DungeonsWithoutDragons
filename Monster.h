bc466413a44f787c4d290e19fb550ba58dff897a
Austin Brown <austinmbrown22@gmail.com>
2019-04-26 20:30:32 -0500

    Finnished Monster

--- Monster.h
+++ Monster.h
@@ -1,8 +1,33 @@
 #ifndef MONSTER_H
 #define MONSTER_H
+#include <string>
 
 class Monster : Character{
-  
+public:
+  Monster();
+  Monster(int, int, Damage, Weapon, string);
+  void setName(string);
+  string getName();
+
+private:
+  string name;
+
 };
+
+Monster::Mosnter() : Character(){
+  name = "Carl";
+}
+
+Monster::Monster(int bH, int mH, Damage dam, Weapon we, string nam) : Character(bH, mH, dam, we){
+  name = nam;
+}
+
+void Monster::setName(string nam){
+  name = nam;
+}
+
+string Monster::getName(){
+  return name;
+}
 
 #endif
