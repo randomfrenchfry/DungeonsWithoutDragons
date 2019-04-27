26fe3cb8035b7110ada5715544afe9a3b3b4b991
Austin Brown <austinmbrown22@gmail.com>
2019-04-26 20:23:17 -0500

    finnish State

--- State.h
+++ State.h
@@ -26,4 +26,67 @@ private:
   int damageMod;
 };
 
+State::State(){
+  baseHealth = 0;
+  healthMod = 0;
+  maxHealth = 0;
+  Damage tmp();
+  damage = tmp;
+  damageMod = 0;
+}
+
+State::State(int bH, int mH, Damage dam){
+  baseHealth = bH;
+  healthMod = 0;
+  maxHealth = mH;
+  damage = dam;
+  damageMod = 0;
+}
+
+void State::setBaseHealth(int bH){
+  baseHealth = bH;
+}
+
+int State::getBaseHealth(){
+  return baseHealth;
+}
+
+void State::setMaxHealth(int mH){
+  maxHealth = mH;
+}
+
+int State::getMaxHealth(){
+  return maxHealth;
+}
+
+void State::heal(int he){
+  baseHealth += he;
+  if(baseHealth > maxHealth){
+    baseHealth = maxHealth;
+  }
+}
+
+void State::hurt(int hurt){
+  baseHealth -= hurt;
+}
+
+void State::Weaken(int we){
+  damageMod -= we;
+}
+
+void State::strengthen(int str){
+  damageMod += str;
+}
+
+void State::setDamage(Damage dam){
+  damage = dam;
+}
+
+void upkeep(){
+  baseHealth += healthMod;
+  if(baseHealth > maxHealth){
+    baseHealth = maxHealth;
+  }
+}
+
 #endif
