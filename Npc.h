23e5584596906034b175b49f7be2062824dcc760
Austin Brown <austinmbrown22@gmail.com>
2019-04-26 20:34:15 -0500

    finnish Npc

--- Npc.h
+++ Npc.h
@@ -3,7 +3,31 @@
 #include "Character.h"
 
 class Npc : Character{
+public:
+  Npc();
+  Npc();
+  void setName();
+  string getName();
+
+private:
+  string name;
 
 };
+
+Npc::Npc() : Character(){
+  name = "Carlos";
+}
+
+Npc::Npc(int bH, int mH, Damage dam, Weapon we, string nam) : Character(bH, mH, dam, we){
+  name = nam;
+}
+
+void Npc::setName(string nam){
+  name = nam;
+}
+
+string Npc::getName(){
+  return name;
+}
 
 #endif
